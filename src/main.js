5f867bfc028d4c88c40cf9ab650bf05ccdea0ece
jw <cjw0515@naver.com>
2019-08-11 00:57:30 +0900

    template change

--- src/main.js
+++ src/main.js
@@ -1,24 +1,51 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
 import store from './store'
-import VueRouter from 'vue-router';
-import { routes } from './router/routes';
-import { index } from './components/index';
+import router from './router'
 
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
 
-// Router
-Vue.use(VueRouter);
-const router = new VueRouter({
-    routes,
-    linkActiveClass: 'open active',
-    scrollBehavior: () => ({ y: 0 }),
-    mode: 'hash'
-});
+import * as filters from './filters' // global filters
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App),
-  components: { App }
+  render: h => h(App)
 })
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,15 @@
-const config = state => state.app.config
-const isLoading = state => state.app.isLoading
-const palette = state => state.app.config.palette
-
-export {
-  isLoading,
-  palette,
-  config
+const getters = {
+  sidebar: state => state.app.sidebar,
+  size: state => state.app.size,
+  device: state => state.app.device,
+  visitedViews: state => state.tagsView.visitedViews,
+  cachedViews: state => state.tagsView.cachedViews,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  introduction: state => state.user.introduction,
+  roles: state => state.user.roles,
+  permission_routes: state => state.permission.routes,
+  errorLogs: state => state.errorLog.logs
 }
+export default getters
