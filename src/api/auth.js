30dc2914c8166d68148b3feee994bbdbbc9a203a
jw <cjw0515@naver.com>
2019-08-18 23:24:00 +0900

    auth api 추가

---
+++ src/api/auth.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  console.log('api/login')
+  const rest = request({
+    url: '/auth/login',
+    method: 'post',
+    data
+    // baseURL: process.env.VUE_APP_BASE_AUTH_API
+  })
+  return rest
+}
+
+export function logout() {
+  return request({
+    url: '/auth/logout',
+    method: 'post'
+  })
+}
