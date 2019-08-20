56a27215935486dcda7abbc4eaff84ee02479986
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-08-20 17:10:14 +0900

    쿠키 expire값 추가

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,7 +8,7 @@ export function getToken() {
 
 export function setToken(token) {
   console.log('cookie/set token')
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, { expires: 1 })
 }
 
 export function removeToken() {
--- src/utils/request.js
+++ src/utils/request.js
@@ -17,10 +17,6 @@ service.interceptors.request.use(
     // do something before request is sent
 
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      // console.log(getToken())
       config.headers['Authorization'] = `Bearer ${getToken()}`
     }
     return config
