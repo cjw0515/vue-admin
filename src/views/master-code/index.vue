46530ca09456631db69e1a510eabfbe2e6b44223
jw <cjw0515@naver.com>
2019-09-11 00:33:13 +0900

    필터 추가

--- src/views/master-code/index.vue
+++ src/views/master-code/index.vue
@@ -1,8 +1,73 @@
 <template>
-  <div>master-code</div>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input
+        v-model="listQuery.title"
+        placeholder="Title"
+        style="width: 200px;"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-select
+        v-model="listQuery.importance"
+        placeholder="Imp"
+        clearable
+        style="width: 90px"
+        class="filter-item"
+      >
+        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-select
+        v-model="listQuery.sort"
+        style="width: 140px"
+        class="filter-item"
+        @change="handleFilter"
+      >
+        <el-option
+          v-for="item in sortOptions"
+          :key="item.key"
+          :label="item.label"
+          :value="item.key"
+        />
+      </el-select>
+      <el-button
+        v-waves
+        class="filter-item"
+        type="primary"
+        icon="el-icon-search"
+        @click="handleFilter"
+      >Search</el-button>
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        type="primary"
+        icon="el-icon-edit"
+        @click="handleCreate"
+      >Add</el-button>
+    </div>
+  </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      listQuery: {
+        page: 1,
+        limit: 20,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      importanceOptions: [1, 2, 3]
+    }
+  },
+  methods: {
+    handleFilter() {
+      this.listQuery.page = 1
+    }
+  }
+}
 </script>
 <style>
 </style>
