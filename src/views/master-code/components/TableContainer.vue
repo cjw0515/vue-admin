ba2b2b428c144dd5cc1418d3c25f7a616d270d98
jw <cjw0515@naver.com>
2019-09-11 20:31:43 +0900

    마스터 테이블 수정

--- src/views/master-code/components/TableContainer.vue
+++ src/views/master-code/components/TableContainer.vue
@@ -1,43 +1,89 @@
 <template>
-  <div>
-    <el-table
-      :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;"
-    >
-      <el-table-column label="test1" prop="test1" align="center" width="80">
-        <template slot-scope="scope">
-          <span>{{ scope.test1 }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="test2" prop="test2" width="150px" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.test2 }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+  <el-table :data="tableData" :row-class-name="tableRowClassName" style="width: 100%">
+    <el-table-column label="Date" width="180">
+      <template slot-scope="scope">
+        <i class="el-icon-time" />
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Name" width="180">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>Name: {{ scope.row.name }}</p>
+          <p>Addr: {{ scope.row.address }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column label="Operations">
+      <template slot-scope="scope">
+        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+        <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 <script>
 export default {
   data() {
     return {
-      list: [
+      tableData: [
         {
-          test1: 'hi',
-          test2: 'hi2'
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          status: true
         },
         {
-          test1: 'hi',
-          test2: 'hi2'
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          status: true
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          status: true
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          status: false
         }
       ]
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    tableRowClassName({ row, rowIndex }) {
+      if (!row.status) {
+        return 'warning-row'
+      }
+      // if (rowIndex === 1) {
+      //   return 'warning-row';
+      // } else if (rowIndex === 3) {
+      //   return 'success-row';
+      // }
+      return ''
     }
   }
 }
 </script>
 <style>
+.el-table .warning-row {
+  background: oldlace;
+}
 
+.el-table .success-row {
+  background: #f0f9eb;
+}
 </style>
