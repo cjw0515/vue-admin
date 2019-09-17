bc5b3c508eb2111e2cec53a6f9df162a7da3772e
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-17 17:09:48 +0900

    수정 로직 추가개발

--- src/views/master-code/components/TableContainer.vue
+++ src/views/master-code/components/TableContainer.vue
@@ -1,39 +1,58 @@
 <template>
-  <el-table
-    :data="tableData"
-    :row-class-name="tableRowClassName"
-    row-key="codeNo"
-    lazy
-    :load="load"
-    :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-    style="width: 100%"
-  >
-    <el-table-column label="코드명" width="250" prop="codeName" />
-    <el-table-column label="코드" width="220">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.codeNo }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="등록일" width="220">
-      <template slot-scope="scope">
-        <i class="el-icon-time" />
-        <span style="margin-left: 10px">{{ scope.row.regDate }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="사용여부" width="220">
-      <template slot-scope="scope">
-        <el-switch v-model="scope.row.status" active-text="사용" inactive-text="사용 안함" />
-      </template>
-    </el-table-column>
-    <el-table-column label="Operations">
-      <template slot-scope="">
-        <el-button type="primary" size="mini">Edit</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table
+      :data="tableData"
+      :row-class-name="tableRowClassName"
+      row-key="codeNo"
+      lazy
+      :load="load"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      style="width: 100%"
+    >
+      <el-table-column label="코드명" width="250" prop="codeName" />
+      <el-table-column label="코드" width="220">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.codeNo }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="등록일" width="220">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span style="margin-left: 10px">{{ dispTime(scope.row.regDate) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="사용여부" width="220">
+        <template slot-scope="scope">
+          <el-switch v-model.lazy="scope.row.status" active-value="1" inactive-value="0" active-text="사용" inactive-text="사용 안함" @change="handleEdit(scope.row)" />
+        </template>
+      </el-table-column>
+      <el-table-column label="Operations">
+        <template slot-scope="{row}">
+          <el-button type="primary" size="mini" @click="handleUpdateName(row)">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 다이얼로그 -->
+    <el-dialog title="코드 수정" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="코드" :label-width="formLabelWidth">
+          {{ form.codeNo }}
+        </el-form-item>
+        <el-form-item label="코드 명" :label-width="formLabelWidth">
+          <el-input v-model="form.codeName" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="handleClickConfirm()">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 <script>
-import { getMasterCodes } from '@/api/master-code'
+import { getMasterCodes, updateMasterCode } from '@/api/master-code'
+import { parseTime } from '@/utils/index'
 
 export default {
   props: {
@@ -45,36 +64,75 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      dialogFormVisible: false,
+      form: {
+        codeName: '',
+        codeNo: ''
+      },
+      tmpRow: {},
+      formLabelWidth: '120px'
+    }
+  },
+  mounted: function() {
   },
   methods: {
-    handleEdit(index, row) {
-      console.log(index, row)
+    async handleEdit(row, modPayload) {
+      const payload = !modPayload ? {
+        codeName: row.codeName,
+        status: row.status
+      } : modPayload
+      try {
+        await updateMasterCode(row.codeNo, payload)
+        this.$notify({
+          title: 'Success!',
+          message: '업데이트 되었습니다!',
+          type: 'success',
+          duration: 2000
+        })
+        return true
+      } catch (err) {
+        row.status = row.status === '1' ? '0' : '1'
+      }
     },
-    handleDelete(index, row) {
-      console.log(index, row)
+    handleUpdateName(row) {
+      // this.temp = Object.assign({}, row) // copy obj
+      // this.temp.timestamp = new Date(this.temp.timestamp)
+      // this.dialogStatus = 'update'
+      // this.dialogFormVisible = true
+      // this.$nextTick(() => {
+      //   this.$refs['dataForm'].clearValidate()
+      // })
+      this.dialogFormVisible = true
+      this.form.codeName = row.codeName
+      this.form.codeNo = row.codeNo
+      this.tmpRow = row
+    },
+    handleClickConfirm() {
+      const payload = {
+        codeName: this.form.codeName,
+        status: this.tmpRow.status
+      }
+      if (this.handleEdit(this.tmpRow, payload)) {
+        this.dialogFormVisible = false
+        this.tmpRow.codeName = this.form.codeName
+      }
     },
     tableRowClassName({ row, rowIndex }) {
-      if (!row.status) {
-        return 'warning-row'
-      }
-      // if (rowIndex === 1) {
-      //   return 'warning-row';
-      // } else if (rowIndex === 3) {
-      //   return 'success-row';
-      // }
-      return ''
+      return row.status === '0' ? 'warning-row' : ''
     },
     async load(row, treeNode, resolve) {
       const res = await getMasterCodes(row.codeNo, row.depth + 1)
       const tmpArr = res.map(el => {
         return {
           ...el,
-          status: el.status === 1,
-          hasChildren: el.depth >= 0
+          hasChildren: el.childCnt > 0
         }
       })
       resolve(tmpArr)
+    },
+    dispTime(time) {
+      return parseTime(time, '{y}-{m}-{d}')
     }
   }
 }
