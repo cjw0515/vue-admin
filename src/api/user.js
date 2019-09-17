0a61b3d8ff0066ecae573598f9d5aed45f5eea72
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-17 17:01:16 +0900

    토큰으로 유저 정보 가져오는 api 수정

--- src/api/user.js
+++ src/api/user.js
@@ -1,15 +1,8 @@
 import request from '@/utils/request'
 
-export function getUserInfo(token) {
-  // return request({
-  //   url: '/user/info',
-  //   method: 'get',
-  //   params: { token },
-  //   baseURL: process.env.VUE_APP_BASE_API
-  // })
+export function getUserInfo() {
   return request({
-    url: `/auth/user/${token}`,
-    method: 'get',
-    params: { token }
+    url: `/auth/user`,
+    method: 'get'
   })
 }
