503e48a1d8dd7327f212aca6dce23fd3e37a8822
jw <cjw0515@naver.com>
2019-09-22 21:43:13 +0900

    페이징처리 완료, 수정로직 작업

--- src/views/age-code/index.vue
+++ src/views/age-code/index.vue
@@ -3,19 +3,19 @@
     <FilterContainer />
     <TableContainer
       v-if="tableData.length > 0"
+      v-loading="listLoading"
       :table-data="tableData"
+      :total="total"
+      :list-query="listQuery"
       @addValue="addAditionalValue"
       @getList="getcodeAges"
     />
   </div>
 </template>
 <script>
-// import Pagination from '@/components/Pagination'
 import FilterContainer from './components/FilterContainer'
 import TableContainer from './components/TableContainer'
-import {
-  getcodeAges
-} from '@/api/age-code'
+import { getcodeAges } from '@/api/age-code'
 
 export default {
   components: { FilterContainer, TableContainer },
@@ -23,12 +23,14 @@ export default {
     return {
       listQuery: {
         page: 1,
-        per_page: 10,
+        perPage: 10,
         importance: undefined,
         title: undefined,
         type: undefined,
         sort: '+id'
       },
+      listLoading: true,
+      total: 0,
       importanceOptions: [1, 2, 3],
       tableData: []
     }
@@ -41,16 +43,24 @@ export default {
       this.listQuery.page = 1
     },
     async getcodeAges(query) {
+      this.listLoading = true
       const { data } = await getcodeAges(query)
-      this.tableData = data
+      this.tableData = this.addAditionalValue(data.list)
+      this.listQuery.perPage = data.perPage
+      this.total = data.total
+      setTimeout(() => {
+        this.listLoading = false
+      }, 0.5 * 1000)
     },
     addAditionalValue(arr) {
       if (arr.length <= 0) return []
       return arr.map(el => {
-        return {
-          ...el,
-          hasChildren: el.childCnt > 0
-        }
+        this.$set(el, 'edit', false)
+        el.originalAge = el.age
+        el.originalGbn = el.gbn
+        el.originalAgeName = el.ageName
+        el.originalStatus = el.status
+        return el
       })
     }
   }
