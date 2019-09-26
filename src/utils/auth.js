8b4061a20ff7c0f7de0e2fadc6b80638b8d0f534
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-09-26 17:51:03 +0900

    쿠키 만료기간 세분화

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,17 @@
 import Cookies from 'js-cookie'
 
 const TokenKey = 'Admin-Token'
+const expireTime = new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
 
 export function getToken() {
+  // const currentToken = Cookies.get(TokenKey)
+  // console.log(getUserInfo())
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
   console.log('cookie/set token')
-  return Cookies.set(TokenKey, token, { expires: 1 })
+  return Cookies.set(TokenKey, token, { expires: expireTime })
 }
 
 export function removeToken() {
