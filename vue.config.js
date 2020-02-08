e6d321922ac1955f64a500cbc08b2863236f52bb
cjw0515 <cjw0515@10x10.co.kr>
2020-02-08 23:24:20 +0900

    빌드 디렉토리 변경

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: './',
-  outputDir: '../backend/dist',
+  outputDir: '../dmap-admin/dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
